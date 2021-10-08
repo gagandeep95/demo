94fa821303dd971d3664ea22690bbb40121817d6
Gagandeep <gagandeep.developer.95@gmail.com>
2021-10-08 11:04:31 -0400

    Added search by name bar component

---
+++ src/components/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../App.css';
+
+function Profile({filteredStudents}) {
+
+      // This method is used to find average of the students 's grades
+    const findAverage = (total, grade, index, arr) => {
+        return total + parseInt(grade)/arr.length;
+    }
+    return (
+        <div>
+           {
+                filteredStudents.map((student, key) => (
+                    <div className="student" key={student.id}>
+                        <figure className="student__pic">
+                            <img src={student.pic} alt="profilepic"/>
+                        </figure>   
+                        <div className="student__info">
+                            <h1>{student.firstName + ' ' + student.lastName}</h1>
+                            <p>Email: {student.email}</p>
+                            <p>Company: {student.company}</p>
+                            <p>Skill: {student.skill}</p>
+                            <p>Average: {student.grades.reduce(findAverage, 0)} %</p>
+                        </div>
+                   </div>        
+                ))
+            } 
+        </div>
+    )
+}
+
+export default Profile
